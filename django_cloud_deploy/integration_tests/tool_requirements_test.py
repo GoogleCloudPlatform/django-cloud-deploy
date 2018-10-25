62397d4aab8eb59637f47bc58ee7fe9a2eb214a9
Enrique Jose Padilla <epad@google.com>
2018-10-25 11:20:58 -0700

    Refactor Requirements Checker. (#17)
    * Refactor Requirements Checker.
    
    * Updated setup.py for PyPI launch.
    
    * Fixed comments from code review
    
    * Fixed redundant line
    
    * Addressing Code Review Comments
    
    * Reviewed comments
    
    * Fix comments

---
+++ django_cloud_deploy/integration_tests/tool_requirements_test.py
@@ -0,0 +1,115 @@
+# Copyright 2018 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Integration tests for tool_requirements.py."""
+import os
+
+from django_cloud_deploy import tool_requirements
+from django_cloud_deploy.cli import io
+from absl.testing import absltest
+
+
+class ToolRequirementsNegativeTest(absltest.TestCase):
+    """This will test all negative cases (not having the requirement)."""
+
+    @classmethod
+    def setUpClass(cls):
+        """Modify the path to exclude the necesary requirments."""
+        cls.temp_path = os.environ['PATH']
+        os.environ['PATH'] = ''
+
+    @classmethod
+    def tearDownClass(cls):
+        os.environ['PATH'] = cls.temp_path
+
+    def setUp(self):
+        self.test_console = io.TestIO()
+
+    def requirement_missing(self, req: tool_requirements.Requirement):
+        with self.assertRaises(
+                tool_requirements.MissingRequirementsError) as cm:
+            tool_requirements._REQUIREMENTS = [req]
+            tool_requirements.check_and_handle_requirements(self.test_console)
+
+        exception = cm.exception
+        self.assertEqual(req.NAME,
+                         exception.missing_requirements[0].name)
+
+    def test_gcloud_missing(self):
+        gcloud = tool_requirements.Gcloud
+        self.requirement_missing(gcloud)
+
+    def test_kubectl_missing(self):
+        kubectl = tool_requirements.Kubectl
+        self.requirement_missing(kubectl)
+
+    def test_cloud_sql_proxy_missing(self):
+        cloud_sql_proxy = tool_requirements.CloudSqlProxy
+        self.test_console.answers.append('n')
+        self.requirement_missing(cloud_sql_proxy)
+
+    def test_docker_missing(self):
+        docker = tool_requirements.Docker
+        self.requirement_missing(docker)
+
+
+class ToolRequirementsPositiveTest(absltest.TestCase):
+    """This will test all positive cases (Having the requirement)."""
+
+    def setUp(self):
+        self.test_console = io.TestIO()
+
+    def has_requirement(self, req: tool_requirements.Requirement):
+        try:
+            tool_requirements._REQUIREMENTS = [req]
+            tool_requirements.check_and_handle_requirements(self.test_console)
+        except tool_requirements.MissingRequirementsError:
+            self.fail('Not suppose to be missing {}'.format(req.Name))
+
+    def test_gcloud(self):
+        Gcloud = tool_requirements.Gcloud
+        self.has_requirement(Gcloud)
+
+    def test_kubectl(self):
+        kubectl = tool_requirements.Kubectl
+        self.has_requirement(kubectl)
+
+    def test_cloud_sql_proxy(self):
+        cloud_sql_proxy = tool_requirements.CloudSqlProxy
+        # This should only download once in our machine unless gcloud overrides
+        self.test_console.answers.append('y')
+        self.has_requirement(cloud_sql_proxy)
+
+    def test_docker(self):
+        docker = tool_requirements.Docker
+        self.has_requirement(docker)
+
+
+class ToolRequirementsHandleTest(absltest.TestCase):
+    """This will test the handle functions of Requirements."""
+
+    def setUp(self):
+        self.test_console = io.TestIO()
+
+    def has_requirement(self, req: tool_requirements.Requirement):
+        try:
+            req.check()
+        except tool_requirements.MissingRequirementsError:
+            self.fail('Not suppose to be missing {}'.format(req.Name))
+
+    def test_cloud_sql_proxy_handle(self):
+        cloud_sql_proxy = tool_requirements.CloudSqlProxy
+        self.test_console.answers.append('y')
+        cloud_sql_proxy.handle(self.test_console)
+        self.has_requirement(cloud_sql_proxy)
---
+++ django_cloud_deploy/tool_requirements.py
@@ -0,0 +1,282 @@
+# Copyright 2018 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Checks the user has the necesarry requirements to run the tool."""
+
+import os
+import shutil
+import stat
+import subprocess
+import sys
+import urllib.request
+
+from typing import List
+
+from django_cloud_deploy.cli import io
+
+
+class UnableToAutomaticallyInstallError(Exception):
+    """Thrown when the handle function is unable to install the requirement."""
+    pass
+
+
+class MissingRequirementError(Exception):
+    """Thrown when a requirement is missing.
+
+    Attributes:
+        name: Name of the requirement.
+        how_to_install_message: Information to help user to install
+            the requirement.
+
+    """
+
+    def __init__(self, name: str, how_to_install_message: str):
+        """Provides information on how to manually install the requirement.
+
+        Args:
+            name: Name of the requirement.
+            how_to_install_message: Information to help user to install
+                the requirement.
+        """
+        self.name = name
+        self.how_to_install_message = how_to_install_message
+
+
+class MissingRequirementsError(Exception):
+    """Thrown when one or more requirements are missing.
+
+    This is used by the caller to provide information to the user on how to
+    manually install missing requirements.
+
+    Attributes:
+        missing_requirements: List of missing requirements.
+    """
+
+    def __init__(self, missing_requirements: List[MissingRequirementError]):
+        """Initializes the MissingRequirementsError.
+
+        Contains a list of the missing requirements with information on
+        how to install all of them manually.
+
+        Args:
+            missing_requirements: List of missing requirements.
+        """
+        self.missing_requirements = missing_requirements
+
+
+class Requirement(object):
+    """Base class for all requirements the tool needs to run."""
+
+    @classmethod
+    def check(cls):
+        """Checks if the requirement is installed.
+
+        Raises:
+            MissingRequirementError: If the requirement is not satisfied.
+        """
+        raise NotImplementedError
+
+    @classmethod
+    def handle(cls, console: io.IO):
+        """Attempts to install the requirement.
+
+        Raises:
+            UnableToAutomaticallyInstall: If the installation fails.
+            NotImplementedError: If we expect the user to manually install the
+                requirement.
+        """
+        raise NotImplementedError
+
+    @classmethod
+    def check_and_handle(cls, console: io.IO):
+        """Checks if requirements is installed, if not, attempts to install it.
+
+        Checks if the requirement is installed.
+        If not, attempts to install the requirement.
+        If unsuccesful, provides information on how to manully install.
+
+        Raises:
+            MissingRequirementError: If the requirement needs to be
+                installed manually.
+        """
+        try:
+            cls.check()
+        except MissingRequirementError as missing_requirement_error:
+            try:
+                cls.handle(console)
+            except (NotImplementedError, UnableToAutomaticallyInstallError):
+                raise missing_requirement_error
+
+
+class Gcloud(Requirement):
+    NAME = 'Gcloud SDK'
+
+    @classmethod
+    def check(cls):
+        """Checks if Gcloud SDK is installed.
+
+        Raises:
+            MissingRequirementError: If the requirement is not found.
+        """
+        if not shutil.which('gcloud'):
+            download_link = 'https://cloud.google.com/sdk/install'
+            msg = ('Please download Google Cloud SDK'
+                   'from {}'.format(download_link))
+            raise MissingRequirementError(cls.NAME, msg)
+
+
+class Docker(Requirement):
+    NAME = 'Docker'
+
+    @classmethod
+    def check(cls):
+        """Checks if Docker is installed.
+
+        Raises:
+            MissingRequirementError: If the requirement is not found.
+        """
+        if not shutil.which('docker'):
+            download_link = 'https://store.docker.com/'
+            msg = 'Please download Docker from {}'.format(download_link)
+            raise MissingRequirementError(cls.NAME, msg)
+
+        try:
+            command = ['docker', 'image', 'ls']
+            subprocess.check_call(command, stdout=subprocess.DEVNULL,
+                                  stderr=subprocess.DEVNULL)
+        except subprocess.CalledProcessError:
+            # TODO: Handle for multiple OS's
+            # TODO: Check output for error message given when user has
+            # not ran the command
+            msg = ('Docker is installed but not correctly set up.'
+                   'Use the following command to fix it: '
+                   'sudo usermod -a -G docker $USER\n'
+                   'Then log out/log back in')
+            raise MissingRequirementError(cls.NAME, msg)
+
+
+class Kubectl(Requirement):
+    NAME = 'Kubectl'
+
+    @classmethod
+    def check(cls):
+        """Checks if Kubectl is installed.
+
+        Raises:
+            MissingRequirementError: If the requirement is not found.
+        """
+        if not shutil.which('kubectl'):
+            msg = 'To install kubectl run: gcloud components install kubectl'
+            raise MissingRequirementError(cls.NAME, msg)
+
+
+class CloudSqlProxy(Requirement):
+    NAME = 'Cloud Sql Proxy'
+
+    @classmethod
+    def check(cls):
+        """Checks if Cloud Sql Proxy is installed.
+
+        Raises:
+            MissingRequirementError: If the requirement is not found.
+        """
+        if not shutil.which('cloud_sql_proxy'):
+            dl_link = 'https://cloud.google.com/sql/docs/mysql/sql-proxy'
+            msg = 'Please download Cloud SQL Proxy from {}'.format(dl_link)
+            raise MissingRequirementError(cls.NAME, msg)
+
+    @classmethod
+    def handle(cls, console: io.IO):
+        """Attempts to install the requirement.
+
+        Raises:
+            UnableToAutomaticallyInstall: If the installation fails.
+            NotImplementedError: If we expect the user to manually install the
+                requirement.
+        """
+        can_download = cls._prompt_download_requirement(console)
+        if can_download:
+            cls._download_cloud_sql_proxy(console)
+        else:
+            raise UnableToAutomaticallyInstallError
+
+    @classmethod
+    def _prompt_download_requirement(cls, console: io.IO) -> bool:
+        while True:
+            answer = console.ask(
+                'Cloud Sql Proxy is required, download? [Y/n]:')
+
+            if not answer or answer.strip().lower() == 'y':
+                return True
+            elif answer.strip().lower() == 'n':
+                return False
+
+    @classmethod
+    def _download_cloud_sql_proxy(cls, console: io.IO):
+        import certifi
+
+        # We will install the proxy where gcloud is
+        gcloud_path = shutil.which('gcloud')
+        if gcloud_path is None:
+            raise UnableToAutomaticallyInstallError
+
+        gcloud_dir = os.path.dirname(gcloud_path)
+        if sys.platform.startswith('linux'):
+            url = 'https://dl.google.com/cloudsql/cloud_sql_proxy.linux.amd64'
+        elif sys.platform.startswith('darwin'):
+            url = 'https://dl.google.com/cloudsql/cloud_sql_proxy.darwin.amd64'
+        else:
+            msg = '{} is not supported, only linux/mac.'.format(sys.platform)
+            raise NotImplementedError(msg)
+
+        console.tell('Downloading Cloud Sql Proxy')
+
+        file_name = 'cloud_sql_proxy'
+        executable_name = os.path.join(gcloud_dir, file_name)
+        with open(executable_name, 'wb') as file:
+            with urllib.request.urlopen(url, cafile=certifi.where()) as data:
+                file.write(data.read())
+
+        st = os.stat(executable_name)
+        os.chmod(executable_name, st.st_mode | stat.S_IRWXU)
+
+
+_REQUIREMENTS = [
+    Gcloud,
+    Docker,
+    Kubectl,
+    CloudSqlProxy
+]
+
+
+def check_and_handle_requirements(console: io.IO):
+    """Checks that requirements are installed. Attempts to install missing ones.
+
+    Args:
+        console: IO, class that handles the input/output with the user.
+    Raises:
+        MissingRequirementsError: Error that contains list of missing
+            requirements.
+    """
+
+    missing_requirement_errors = []
+    for req in _REQUIREMENTS:
+        try:
+            req.check_and_handle(console)
+        except MissingRequirementError as e:
+            missing_requirement_errors.append(e)
+
+    if missing_requirement_errors:
+        raise MissingRequirementsError(
+            missing_requirements=missing_requirement_errors)
