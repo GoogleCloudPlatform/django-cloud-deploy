58e24d050eae96d1fb9680ef458aef324351f79d
Brian Quinlan <brian@sweetapp.com>
2018-11-16 00:45:22 -0800

    Update the correct prompt.

--- django_cloud_deploy/cli/prompt.py
+++ django_cloud_deploy/cli/prompt.py
@@ -478,8 +478,9 @@ class DjangoFilesystemPath(Prompt):
                 ' ', '-'))
 
         while True:
-            console.tell('{} Enter a directory or leave blank to use'.format(
-                step_prompt))
+            console.tell(
+                ('{} Enter a new directory path to store project source, '
+                 'or leave blank to use').format(step_prompt))
             directory = console.ask('[{}]: '.format(default_directory))
             if not directory.strip():
                 directory = default_directory
@@ -525,8 +526,8 @@ class DjangoFilesystemPathUpdate(Prompt):
 
         while True:
             console.tell(
-                ('{} Enter a new directory path to store project source, '
-                 'or leave blank to use').format(step_prompt))
+                ('{} Enter the directory of the Django project you want to '
+                 'update:'.format(step_prompt)))
             directory = console.ask('[{}]: '.format(default_directory))
             if not directory.strip():
                 directory = default_directory
