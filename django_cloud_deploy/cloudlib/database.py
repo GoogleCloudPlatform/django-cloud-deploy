4efc1f5905886d4d2a8165e67ff3b9e1fb3253a9
Xiaofei Wang <6218006+wangxf123456@users.noreply.github.com>
2019-03-08 14:13:50 -0800

    Fix integration test about database reusing (#274)
    * Fix test.
    
    * Fix
    
    * Fix
    
    * Resolve comments.
    
    * Fix test.

--- django_cloud_deploy/cloudlib/database.py
+++ django_cloud_deploy/cloudlib/database.py
@@ -142,6 +142,24 @@ class DatabaseClient(object):
         """
         # See:
         # https://cloud.google.com/sql/docs/mysql/admin-api/v1beta4/databases/insert
+        request = self._sqladmin_service.databases().get(
+            project=project_id, instance=instance, database=database)
+        response = []
+        try:
+            response = request.execute()
+        except errors.HttpError as e:
+            if e.resp.status == 404:
+                # The database we would like to create does not exist yet. This
+                # is what we want.
+                pass
+            else:
+                raise DatabaseError(
+                    ('Unexpected response when getting status of database '
+                     '{!r}: [{!r}]').format(database, response))
+        # This means the database already exist. In this case we do not need
+        # to create the same database again.
+        if 'name' in response:
+            return
         request = self._sqladmin_service.databases().insert(
             project=project_id,
             instance=instance,
