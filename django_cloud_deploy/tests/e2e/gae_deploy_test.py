a55149698386ec6730cc55db7b66133c10d2a803
Xiaofei Wang <6218006+wangxf123456@users.noreply.github.com>
2019-03-13 15:07:06 -0700

    Fix copyrights. (#299)

--- django_cloud_deploy/tests/e2e/gae_deploy_test.py
+++ django_cloud_deploy/tests/e2e/gae_deploy_test.py
@@ -1,6 +1,6 @@
 # Copyright 2019 Google LLC
 #
-# Licensed under the Apache License, Versconsolen 2.0 (the "License");
+# Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
@@ -8,9 +8,9 @@
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITconsoleNS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissconsolens and
-# limitatconsolens under the License.
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 """End to end test for create and deploy new project using Google App Engine."""
 
 import os
