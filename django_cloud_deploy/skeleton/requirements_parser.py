017e5ddbf9e9e710256fbb25d2999f7cf74344ab
Xiaofei Wang <6218006+wangxf123456@users.noreply.github.com>
2019-03-19 10:26:19 -0700

    Add a parser for requirements.txt. (#300)
    * Add requirements parser.
    
    * Add a parser for requirements.txt.
    
    * Fix.
    
    * Fix lint.
    
    * Handle more kinds of requirements.
    
    * Fix lint.
    
    * Resolve comments.
    
    * Fix lint.
    
    * Resolve comments.

---
+++ django_cloud_deploy/skeleton/requirements_parser.py
@@ -0,0 +1,78 @@
+# Copyright 2019 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""A module help parse requirements.txt."""
+
+import os
+import re
+from typing import Set
+
+
+def parse_line(line: str) -> str:
+    """Parse a single line in requirements.txt.
+
+    Note that this function only returns the package that is required, without
+    version restrictions
+    e.g. "google-cloud-bigquery>1.2.3" => "google-cloud-bigquery".
+
+    Args:
+        line: A line in requirements.txt.
+
+    Returns:
+        The package requirement contained on the line
+        e.g. "google-cloud-bigquery".
+    """
+
+    # See
+    # https://pip.pypa.io/en/stable/reference/pip_install/#requirement-specifiers
+    return re.split(r'[;\[~>=<]', line)[0].strip()
+
+
+def parse(path: str) -> Set[str]:
+    """Parses requirements given the absolute path of a requirements.txt.
+
+    Note that this function only returns the package that is required, without
+    version restrictions
+    e.g. "google-cloud-bigquery>1.2.3" => "google-cloud-bigquery".
+
+    Args:
+        path: Absolute path of a requirements.txt.
+
+    Returns:
+        The set of packages contained in the given "requirements.txt".
+    """
+
+    results = set()
+    if not os.path.exists(path):
+        return results
+
+    dir_path = os.path.dirname(path)
+    with open(path) as requirements_file:
+        lines = requirements_file.read().splitlines()
+        for line in lines:
+            line = line.strip()
+            # This does not handle the full grammar, for example, line
+            # continuation, package from a url, or package from a local path
+            # TODO: Support more kinds of packages
+            if not line or line.startswith('#'):
+                continue
+            elif line.startswith('-r'):
+                # This will install a list of requirements specified in a file
+                sub_requirements_path = line.split(' ')[-1]
+                results = results.union(
+                    parse(os.path.join(dir_path, sub_requirements_path)))
+            else:
+                results.add(parse_line(line))
+    return results
+
---
+++ django_cloud_deploy/tests/unit/skeleton/requirements_parser_test.py
@@ -0,0 +1,101 @@
+# Copyright 2018 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Unit test for django_cloud_deploy/skeleton/requirements_parser.py."""
+
+import os
+import shutil
+import tempfile
+import unittest
+
+from django_cloud_deploy.skeleton import requirements_parser
+
+
+class RequirementsParserTest(unittest.TestCase):
+    """Unit test for django_cloud_deploy/skeleton/requirements_parser.py."""
+
+    def setUp(self):
+        super().setUp()
+        self._project_dir = tempfile.mkdtemp()
+
+    def tearDown(self):
+        super().tearDown()
+        shutil.rmtree(self._project_dir)
+
+    def test_parse_single_line(self):
+        requirements = [
+            'six', 'urllib3', 'Django', 'backoff', 'mysqlclient', 'gunicorn',
+            'wheel', 'google-cloud-logging'
+        ]
+        lines = [
+            'six==1.2', 'urllib3>=4.5.6', 'Django<=7.8', 'backoff',
+            'mysqlclient~=1.3.4', 'gunicorn[1,7.8, 2.3.4]', 'wheel   == 1.2.3',
+            'google-cloud-logging; sys_platform == \'win32\''
+        ]
+        for i in range(len(lines)):
+            self.assertEqual(
+                requirements_parser.parse_line(lines[i]), requirements[i])
+
+    def test_parse_requirements(self):
+        requirements = ['six', 'urllib3', 'Django', 'backoff']
+        lines = ['six==1.2', 'urllib3>=4.5.6', 'Django<=7.8', 'backoff']
+        requirements_file_path = os.path.join(self._project_dir,
+                                              'requirements.txt')
+        with open(requirements_file_path, 'wt') as f:
+            f.write('\n'.join(lines))
+        results = requirements_parser.parse(requirements_file_path)
+        for requirement in requirements:
+            self.assertIn(requirement, results)
+
+    def test_parse_requirements_with_comments(self):
+        requirements = ['six', 'urllib3', 'Django', 'backoff']
+        lines = [
+            'six==1.2', '# Comment', 'urllib3>=4.5.6', 'Django<=7.8', 'backoff'
+        ]
+        requirements_file_path = os.path.join(self._project_dir,
+                                              'requirements.txt')
+        with open(requirements_file_path, 'wt') as f:
+            f.write('\n'.join(lines))
+        results = requirements_parser.parse(requirements_file_path)
+        for requirement in requirements:
+            self.assertIn(requirement, results)
+        self.assertEqual(len(requirements), len(results))
+
+    def test_parse_requirements_with_empty_lines(self):
+        requirements = ['six', 'urllib3', 'Django', 'backoff']
+        lines = ['six==1.2', '    ', 'urllib3>=4.5.6', 'Django<=7.8', 'backoff']
+        requirements_file_path = os.path.join(self._project_dir,
+                                              'requirements.txt')
+        with open(requirements_file_path, 'wt') as f:
+            f.write('\n'.join(lines))
+        results = requirements_parser.parse(requirements_file_path)
+        for requirement in requirements:
+            self.assertIn(requirement, results)
+        self.assertEqual(len(requirements), len(results))
+
+    def test_parse_requirements_recursively(self):
+        requirements = ['six', 'urllib3', 'Django', 'backoff']
+        lines1 = ['six==1.2', 'urllib3>=4.5.6']
+        lines2 = ['-r requirements1.txt', 'Django<=7.8', 'backoff']
+        requirements_file_path1 = os.path.join(self._project_dir,
+                                               'requirements1.txt')
+        with open(requirements_file_path1, 'wt') as f:
+            f.write('\n'.join(lines1))
+        requirements_file_path2 = os.path.join(self._project_dir,
+                                               'requirements2.txt')
+        with open(requirements_file_path2, 'wt') as f:
+            f.write('\n'.join(lines2))
+        results = requirements_parser.parse(requirements_file_path2)
+        for requirement in requirements:
+            self.assertIn(requirement, results)
+        self.assertEqual(len(requirements), len(results))
